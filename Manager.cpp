e681e48ec1a76cbf97b0d40c8e0de124a4f66fe0
cateliu <cate_liu_hust@163.com>
2022-01-11 19:09:36 +0800

    增加机房预约系统的基础代码

---
+++ Manager.cpp
@@ -0,0 +1,44 @@
+#include "Manager.h"
+#include <iostream>
+
+Manager::Manager()
+{
+	
+}
+
+// 有参构造
+Manager::Manager(string name, string pwd)
+{
+	this->m_Name = name;
+	this->m_Pwd = pwd;
+}
+
+// 显示菜单
+void Manager::operMenu()
+{
+
+}
+
+// 添加账号
+void Manager::addPerson()
+{
+
+}
+
+// 查看账号
+void Manager::showPerson()
+{
+
+}
+
+// 查看机房信息
+void Manager::showComputer()
+{
+
+}
+
+// 清空预约信息
+void Manager::cleanFile()
+{
+
+}
